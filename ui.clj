f946b6b948b8713204871ae3e1cf6559b6641374
Zak Wilson <zak.wilson@gmail.com>
2009-01-31 13:39:07 +0100

    Counter works.

--- ui.clj
+++ ui.clj
@@ -1,66 +1,71 @@
 (ns imagesieve.ui
   (:use imagesieve.logic imagesieve.image zutil.util)
-  (:import (javax.swing JFileChooser JFrame JPanel JTextField UIManager JButton JOptionPane WindowConstants)
+  (:import (javax.swing JFileChooser JFrame JPanel JLabel UIManager JButton JOptionPane WindowConstants)
            (java.awt.event ActionListener)
            (java.io File))
   (:gen-class))
-
+ 
 (in-ns 'imagesieve.ui)
-
-(when-not (= "com.sun.java.swing.plaf.gtk.GTKLookAndFeel" 
+ 
+(when-not (= "com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
              (UIManager/getSystemLookAndFeelClassName))
   (UIManager/setLookAndFeel (UIManager/getSystemLookAndFeelClassName)))
-
-
-(def *count* (agent 0))
-
-(def *max* (agent 0))
-
-(def *status* (agent (JTextField. "0/0 completed")))
-
+ 
+ 
+(def *count* (ref 0))
+ 
+(def *max* (atom 0))
+ 
+(def *status* (JLabel. "0/0 completed"))
+ 
 (defn update-counter [& args]
-  (send-off *count* inc)
-  (send-off *status* (fn [textfield & args]
-                       (.setText textfield
-                                 (str @*count* "/" @*max* " completed"))
-                       textfield)))
+  (dosync (alter *count* inc)
+          (.setText *status*
+                    (str @*count* "/" @*max* " completed"))))
 
+ 
 (defn choose-and-go []) ;placeholder
-
+ 
 (def frame (doto (JFrame. "Greyscale Converter")
              (.add (doto (JPanel.)
                      (.add (doto (JButton. "Convert a folder to greyscale")
                              (.addActionListener
                               (proxy [ActionListener] []
                                 (actionPerformed [e] (choose-and-go))))))
-                     (.add @*status*)))
+                     (.add *status*)))
              (.setDefaultCloseOperation (WindowConstants/EXIT_ON_CLOSE))
              (.pack)))
-
+ 
 (def chooser (doto (JFileChooser.)
                (.setFileSelectionMode (JFileChooser/DIRECTORIES_ONLY))))
-
+ 
 (defn process-and-update [img]
   (make-grey! img)
   (update-counter)
   img)
 
+(defn do-processing [state path]
+ (doall (process-dir process-and-update
+                     path
+                     (str path "-grey/")))
+ nil)
+
+(def *processor* (agent nil))
+
 (defn error->str [e]
   (str (.getMessage e) ", "))
-
-(defn choose-and-go [] 
+ 
+(defn choose-and-go []
   (let [status (.showOpenDialog chooser frame)]
     (when (= (JFileChooser/APPROVE_OPTION) status)
       (try
        (let [path (.getAbsolutePath (.getSelectedFile chooser))
              dir (File. path)]
          (if (and (.exists dir) (.isDirectory dir))
-           (do (send-off *max* identity* (count (.list dir)))
-               (send-off *count* identity* -1)
+           (do (swap! *max* identity* (count (.list dir)))
+               (dosync (ref-set  *count* -1))
                (update-counter)
-               (doall (process-dir process-and-update ;shouldn't be lazy, but doesn't always complete
-                                   path
-                                   (str path "-grey/"))))
+               (send-off *processor* do-processing path))
           (JOptionPane/showMessageDialog nil
                                          "Sorry, I couldn't find the directory you asked for"
                                          "Alert"
@@ -69,7 +74,6 @@
                                                          (.getMessage e)
                                                          "Error"
                                                          (JOptionPane/ERROR_MESSAGE)))))))
-
-(defn -main [] 
+ 
+(defn -main []
   (. frame show))
-;  (require 'zutil.repl))
