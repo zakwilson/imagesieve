85bb17ca93ea130e9e100d6d22d6a5ac07b99333
Zak Wilson <zak.wilson@gmail.com>
2009-01-31 14:09:47 +0100

    Copyright, better error messages

--- ui.clj
+++ ui.clj
@@ -1,3 +1,11 @@
+;   Copyright (c) Zachary Wilson. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file COPYING at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
 (ns imagesieve.ui
   (:use imagesieve.logic imagesieve.image zutil.util)
   (:import (javax.swing JFileChooser JFrame JPanel JLabel UIManager JButton JOptionPane WindowConstants)
@@ -10,8 +18,13 @@
 (when-not (= "com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
              (UIManager/getSystemLookAndFeelClassName))
   (UIManager/setLookAndFeel (UIManager/getSystemLookAndFeelClassName)))
- 
- 
+
+(defn error-message [e]
+  (JOptionPane/showMessageDialog nil
+                                 (.getMessage e)
+                                 "Error"
+                                 (JOptionPane/ERROR_MESSAGE)))
+  
 (def *count* (ref 0))
  
 (def *max* (atom 0))
@@ -45,16 +58,14 @@
   img)
 
 (defn do-processing [state path]
- (doall (process-dir process-and-update
-                     path
-                     (str path "-grey/")))
+ (try (doall (process-dir process-and-update
+                          path
+                          (str path "-grey/")))
+      (catch Exception e (error-message e)))
  nil)
 
 (def *processor* (agent nil))
 
-(defn error->str [e]
-  (str (.getMessage e) ", "))
- 
 (defn choose-and-go []
   (let [status (.showOpenDialog chooser frame)]
     (when (= (JFileChooser/APPROVE_OPTION) status)
@@ -65,15 +76,14 @@
            (do (swap! *max* identity* (count (.list dir)))
                (dosync (ref-set  *count* -1))
                (update-counter)
-               (send-off *processor* do-processing path))
+               (send-off *processor* do-processing path)
+               (when (agent-errors *processor*)
+                 (error-message (first (agent-errors *processor*)))))
           (JOptionPane/showMessageDialog nil
                                          "Sorry, I couldn't find the directory you asked for"
                                          "Alert"
                                          (JOptionPane/ERROR_MESSAGE))))
-       (catch Exception e (JOptionPane/showMessageDialog nil
-                                                         (.getMessage e)
-                                                         "Error"
-                                                         (JOptionPane/ERROR_MESSAGE)))))))
+       (catch Exception e (error-message e))))))
  
 (defn -main []
   (. frame show))
